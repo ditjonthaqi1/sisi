f45d3b4e6495ae4c06001a4e5649b71724520ab3
Eltion Musa <eltimusa4@gmail.com>
2020-06-12 19:40:59 +0200

    Default Commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -30,6 +30,6 @@ router.post('/createroom',midi,rc.createRoom.bind(rc))
 router.post('/joinroom/:id',midi,rc.joinRoom.bind(rc))
 router.post('/auth',midi, (req, res) => { res.send({status:"OK"})});
 router.post('/attendance/:id',midi,rc.attendanceCal.bind(rc))
-router.post('/leaveroom/:id',midi,uc.leaveRoom.bind(uc))
+router.post('/leaveroom/:id',midi,rc.leaveRoom.bind(rc))
 
 module.exports = router
