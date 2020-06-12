d9d18bedc9f9080b8885a773737beadb800303ae
Eltion Musa <eltimusa4@gmail.com>
2020-06-12 22:25:16 +0200

    Default Commit

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express')
 const userRouter = require('./routes/userRoute')
 const {hash} = require('bcrypt')
 const app =  express()
+const path = require('path')
+
+const publicdict = path.join(__dirname)
+
+app.use(express.static(publicdict))
 app.use(express.json())
+
 
 require('./db')
 const hbs = require('hbs')
